581f4ffe7c7e59e53c9acdeea20a9910802aaf4b
Ashlee Yin <80716410+awangran@users.noreply.github.com>
2022-08-17 04:05:15 +0000

    lastlast

--- ip.js
+++ ip.js
@@ -7,11 +7,10 @@ $.get("https://ipinfo.io", function(response) {
   var weather = document.getElementById("question");
   ip.innerHTML = response.ip
   weather.innerHTML = response.country + ", " + response.city + ", " + response.region
-   var nearby = document.getElementById("nearby");
   if (response.country == "US") {
     maybe()
   }else{
-    nearby.innerHTML = "No"
+    nearby.innerHTML = "No";
   }
   function maybe() {
     if ((response.region == "Mississippi") || (response.region == "D.C.") || (response.region == "Nevada")|| (response.region == "Louisiana") || (response.region == "New York") || (response.region == "New Mexico") || (response.region == "Florida") || (response.region == "Tennessee") || (response.region == "North Carolina") || (response.region == "Alabama") || (response.region == "Georgia") || (response.region == "Michigan") || (response.region == "Illinois") || (response.region == "California") || (response.region == "Washington") || (response.region == "New Jersey") || (response.region == "New Mexico") || (response.region == "Texas")) {
@@ -21,10 +20,12 @@ $.get("https://ipinfo.io", function(response) {
     }
   }
   
+  
+
+  
   $.get(`https://fourtonfish.com/hellosalut/?ip=${response.ip}`, function(response) {
     greet.innerHTML = response.hello
   }, "json")
 
 }, "json")
-
 
