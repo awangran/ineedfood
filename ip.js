e43dabb260b6a3e9533dc99eb65baf556a46fdc5
Ashlee Yin <80716410+awangran@users.noreply.github.com>
2022-08-17 02:54:38 +0000

    last

--- ip.js
+++ ip.js
@@ -6,8 +6,25 @@ $.get("https://ipinfo.io", function(response) {
   var greet = document.getElementById("greeting");
   var weather = document.getElementById("question");
   ip.innerHTML = response.ip
-  weather.innerHTML = "Peek a Boo! I see you in " + response.city + ", " + response.region
+  weather.innerHTML = response.country + ", " + response.city + ", " + response.region
+   var nearby = document.getElementById("nearby");
+  if (response.country == "US") {
+    maybe()
+  }else{
+    nearby.innerHTML = "No"
+  }
+  function maybe() {
+    if ((response.region == "Mississippi") || (response.region == "D.C.") || (response.region == "Nevada")|| (response.region == "Louisiana") || (response.region == "New York") || (response.region == "New Mexico") || (response.region == "Florida") || (response.region == "Tennessee") || (response.region == "North Carolina") || (response.region == "Alabama") || (response.region == "Georgia") || (response.region == "Michigan") || (response.region == "Illinois") || (response.region == "California") || (response.region == "Washington") || (response.region == "New Jersey") || (response.region == "New Mexico") || (response.region == "Texas")) {
+      nearby.innerHTML = "Yes"
+    } else{
+      nearby.innerHTML = "No"
+    }
+  }
+  
   $.get(`https://fourtonfish.com/hellosalut/?ip=${response.ip}`, function(response) {
     greet.innerHTML = response.hello
   }, "json")
+
 }, "json")
+
+
